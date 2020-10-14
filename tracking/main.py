02cbf465b48e14ac7c867bbd8b30779ec2fde0e1
dewkul <jirapat0811@gmail.com>
2020-10-14 23:06:07 +0700

    Draft - transformation

--- tracking/main.py
+++ tracking/main.py
@@ -1,13 +1,78 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
+
+result = dict()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+"""
+    return 
+    #1
+    {
+        nodeId: {
+            name: "",
+            edges: [],
+            covidEdges: [],
+        }
+    }
+    #2
+    {
+        name: {
+            edges: [],
+            covidEdges: [],
+        }
+    }
+"""
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+
+# r_json = {
+#     "a": {
+#         "b": 1,
+#         "c": 0
+#     }
+# }
+
+# for name, people in r_json.items():
+#     temp = {
+#         "edges": list(),
+#         "covidEdges": list()
+#     }
+
+#     for p_name, is_covid in people.items():
+#         temp['edges'].append(
+#             p_name) if is_covid else temp['covidEdges'].append(p_name)
+
+#     result[name] = temp
+#     print(result)
+
+
+@app.get("/graph")
+async def transform_graph():
+    # r = requests.get("")
+    # r_json = r.json()
+
+    r_json = {
+        "a": {
+            "b": 1,
+            "c": 0
+        }
+    }
+
+    for name, people in r_json.items():
+        temp = {
+            "edges": list(),
+            "covidEdges": list()
+        }
+
+        for p_name, is_covid in people.items():
+            temp['edges'].append(
+                p_name) if is_covid else temp['covidEdges'].append(p_name)
+
+        result[name] = temp
+
+    return result
