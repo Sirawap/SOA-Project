5784d94739a6f28fb536972aead389ecf658ff4f
Pawaris Lertchaiprasert <60090027@kmitl.ac.th>
2020-10-15 10:11:30 +0700

    get request

--- Person.py
+++ Person.py
@@ -18,23 +18,36 @@ class Person(object):
     def getId(self):
         return self.customerId
 
+    def getContactLog(self):
+        return self.contact_log
+
+    def getCovidLog(self):
+        return self.covid_contact_log
+
     def setContactLog(self,Log):
         self.contact_log.append(Log)
 
     def setCovidLog(self,Log):
         self.covid_contact_log.append(Log)
 
+a=Person('a',10,10)
+b=Person('b',10,10)
+c=Person('c',10,10)
+people.append(a)
+people.append(b)
+people.append(c)
+
 # REST endpoint: GET test1
 # just returns OK
 
 @app.route('/contact', methods=['GET'])
 def getContact():
-    Log = [['b',0],['c',0],['d',1]]
-    # p = Person('a',100,100,{'b':0, 'c':0, 'd':1},{'b':1})
-    return jsonify({
-            'personId' : 'a', 
-            'contact' : Log
-        })
+    message = []
+    for person in people :
+        message.append({"name":person.customerId,"contact":person.contact_log,"covid": person.covid_contact_log})
+    message = jsonify(message)
+
+    return message
 
 @app.route('/contact', methods=['POST'])
 def postContact():
@@ -59,6 +72,26 @@ def postContact():
     people.append(person)
 
     return "new person added"
+
+@app.route('/contact', methods=['PUT'])
+def putContact():
+    #extract data
+    data = request.json
+    id = data['id']
+    normal_contact_list = data['normal']
+    covid_contact_list = data['covid']
+
+    #create normal and covid contact log
+    nlog = Log(normal_contact_list)
+    clog = Log(covid_contact_list)
+
+    #update contact of user with the id
+    for person in people:
+        if(person.getId == id):
+            person.contact_log.append(nlog)
+            person.contact_log.append(clog)
+    
+    return "update person contact"
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
