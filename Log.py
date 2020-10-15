ef14aed09d2e6a1f6f69276e5286de7bc2fde645
Pawaris Lertchaiprasert <60090027@kmitl.ac.th>
2020-10-15 11:06:28 +0700

    put request

--- Log.py
+++ Log.py
@@ -1,12 +1,14 @@
 import datetime
 
 class Log(object):
+
     def __init__(self, contact):
         self.contact = contact
         self.time = datetime.datetime.utcnow().isoformat()
 
     def  getContact(self):
         return self.contact
+        
     def setContact(self,contact):
-        self.contact = contact
+        self.contact.append(contact)
         
--- Person.py
+++ Person.py
@@ -1,7 +1,7 @@
 # Class for person with contact and covid contact log
 from flask import Flask,jsonify # server
 from flask import request
-import Log
+from Log import Log
 # Preparation of Flask server (micro web framework)
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -12,30 +12,28 @@ class Person(object):
         self.deviceId = deviceId
         self.x = x
         self.y = y
-        self.contact_log = []
-        self.covid_contact_log = []
+        self.contact_log = Log([])
+        self.covid_contact_log = Log([])
 
     def getId(self):
         return self.deviceId
 
     def getContactLog(self):
-        return self.contact_log
+        return self.contact_log.getContact()
 
     def getCovidLog(self):
-        return self.covid_contact_log
+        return self.covid_contact_log.getContact()
 
     def setContactLog(self,Log):
-        self.contact_log.append(Log)
+        self.contact_log = Log
 
     def setCovidLog(self,Log):
-        self.covid_contact_log.append(Log)
+        self.covid_contact_log = Log
 
-a=Person('a',10,10)
-b=Person('b',10,10)
-c=Person('c',10,10)
-people.append(a)
-people.append(b)
-people.append(c)
+# a=Person('a',10,10)
+# c=Person('c',10,10)
+# people.append(a)
+# people.append(c)
 
 # REST endpoint: GET test1
 # just returns OK
@@ -44,7 +42,7 @@ people.append(c)
 def getContact():
     message = []
     for person in people :
-        message.append({"name":person.customerId,"contact":person.contact_log,"covid": person.covid_contact_log})
+        message.append({"name":person.deviceId,"contact": person.getContactLog(),"covid": person.getCovidLog()})
     message = jsonify(message)
 
     return message
@@ -60,8 +58,14 @@ def postContact():
     covid_contact_list = data['covid']
     
     #create normal and covid contact log
-    nlog = Log(normal_contact_list)
-    clog = Log(covid_contact_list)
+    # nlog = Log(normal_contact_list)
+    # clog = Log(covid_contact_list)
+    
+    nlog = Log([])
+    nlog.setContact(normal_contact_list)
+    
+    clog = Log([])
+    clog.setContact(covid_contact_list)
     
     #define person
     person = Person(id,x,y)
@@ -71,25 +75,25 @@ def postContact():
     #add new person to the list of people
     people.append(person)
 
-    return "new person added"
+    return "new person added" + str(nlog)    
 
 @app.route('/contact', methods=['PUT'])
 def putContact():
     #extract data
     data = request.json
     id = data['id']
+    x = data['x']
+    y = data['y']
     normal_contact_list = data['normal']
     covid_contact_list = data['covid']
 
-    #create normal and covid contact log
-    nlog = Log(normal_contact_list)
-    clog = Log(covid_contact_list)
-
     #update contact of user with the id
     for person in people:
-        if(person.getId == id):
-            person.contact_log.append(nlog)
-            person.contact_log.append(clog)
+        if(person.getId() == id):
+            person.x = x
+            person.y = y
+            person.contact_log.setContact(normal_contact_list)
+            person.covid_contact_log.setContact(covid_contact_list)
     
     return "update person contact"
     
