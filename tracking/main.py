8303a35ba40e765d1688b93b641e3f6d562097b7
dewkul <jirapat0811@gmail.com>
2020-10-15 14:33:05 +0700

    Draft v2

--- tracking/main.py
+++ tracking/main.py
@@ -11,16 +11,7 @@ async def root():
     return {"message": "Hello World"}
 
 """
-    return 
-    #1
-    {
-        nodeId: {
-            name: "",
-            edges: [],
-            covidEdges: [],
-        }
-    }
-    #2
+    return format
     {
         name: {
             edges: [],
@@ -30,52 +21,65 @@ async def root():
 """
 
 
-# r_json = {
-#     "a": {
-#         "b": 1,
-#         "c": 0
-#     }
-# }
-
-# for name, people in r_json.items():
-#     temp = {
-#         "edges": list(),
-#         "covidEdges": list()
-#     }
-
-#     for p_name, is_covid in people.items():
-#         temp['edges'].append(
-#             p_name) if is_covid else temp['covidEdges'].append(p_name)
-
-#     result[name] = temp
-#     print(result)
-
-
 @app.get("/graph")
 async def transform_graph():
     # r = requests.get("")
     # r_json = r.json()
 
-    r_json = {
-        "a": {
-            "b": 1,
-            "c": 0
-        }
-    }
+    # r_json = {
+    #     "a": {
+    #         "b": 1,
+    #         "c": 0
+    #     }
+    # }
 
-    for name, people in r_json.items():
+    r_json = [
+        {
+            "contact": [
+                {
+                    "contact": "b",
+                    "time": "14.30"
+                }
+            ],
+            "covid": [
+                {
+                    "contact": "c",
+                    "time": "14.30"
+                }
+            ],
+            "id": "a"
+        }
+    ]
+
+    for host in r_json:
         temp = {
-            "edges": list(),
+            "normalEdges": list(),
             "covidEdges": list()
         }
 
-        for p_name, is_covid in people.items():
-            temp['edges'].append(
-                p_name) if is_covid else temp['covidEdges'].append(p_name)
+        for person in host["contact"]:
+            temp['covidEdges'].append(person['contact'])
 
-        result[name] = temp
+        for person in host["covid"]:
+            temp['normalEdges'].append(person['contact'])
+
+        result[host['id']] = temp
 
     return result
+
+    # for name, people in r_json.items():
+    #     temp = {
+    #         "edges": list(),
+    #         "covidEdges": list()
+    #     }
+
+    #     for p_name, is_covid in people.items():
+    #         temp['edges'].append(
+    #             p_name) if is_covid else temp['covidEdges'].append(p_name)
+
+    #     result[name] = temp
+
+    # return result
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="0.0.0.0", port=8080)
